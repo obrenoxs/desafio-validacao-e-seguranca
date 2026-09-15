d091c61d5572bf61e17c9e4f5644ec5d5a1cf9ff
Breno <breno.joaosalvadorperez200@gmail.com>
2026-09-15 10:13:09 -0300

    feat: add findAll method to CityService

--- src/main/java/com/devsuperior/bds04/services/CityService.java
+++ src/main/java/com/devsuperior/bds04/services/CityService.java
@@ -4,8 +4,12 @@ import com.devsuperior.bds04.dto.CityDTO;
 import com.devsuperior.bds04.entities.City;
 import com.devsuperior.bds04.repositories.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CityService {
@@ -21,4 +25,13 @@ public class CityService {
         city = repository.save(city);
         return new CityDTO(city);
     }
+
+    @Transactional(readOnly = true)
+    public List<CityDTO> findAll() {
+        List<City> list = repository.findAll(Sort.by("name"));
+        List<CityDTO> listDTO = list.stream().map((x -> new CityDTO(x))).collect(Collectors.toList());
+
+        return listDTO;
+    }
+
 }
