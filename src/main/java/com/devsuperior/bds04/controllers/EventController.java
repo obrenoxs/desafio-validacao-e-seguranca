39a66063b696970bd3f6b14ed7d8a18dc20c4e57
Breno <breno.joaosalvadorperez200@gmail.com>
2026-09-15 11:19:51 -0300

    feat: add validation rules to EventDTO

--- src/main/java/com/devsuperior/bds04/controllers/EventController.java
+++ src/main/java/com/devsuperior/bds04/controllers/EventController.java
@@ -2,6 +2,7 @@ package com.devsuperior.bds04.controllers;
 
 import com.devsuperior.bds04.dto.EventDTO;
 import com.devsuperior.bds04.services.EventService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -27,7 +28,7 @@ public class EventController {
 
     @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
     @PostMapping
-    public ResponseEntity<EventDTO> insert(@RequestBody EventDTO dto) {
+    public ResponseEntity<EventDTO> insert(@Valid @RequestBody EventDTO dto) {
         EventDTO eventDTO = service.insert(dto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
                 .buildAndExpand(eventDTO.getId()).toUri();
