7f012bce2fb16a44ca0db05c496eb942fd87ea07
Breno <breno.joaosalvadorperez200@gmail.com>
2026-09-15 10:46:44 -0300

    feat: add EventController endpoints

---
+++ src/main/java/com/devsuperior/bds04/controllers/EventController.java
@@ -0,0 +1,34 @@
+package com.devsuperior.bds04.controllers;
+
+import com.devsuperior.bds04.dto.EventDTO;
+import com.devsuperior.bds04.services.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(value = "/events")
+public class EventController {
+
+    @Autowired
+    private EventService service;
+
+    @GetMapping
+    public ResponseEntity<Page<EventDTO>> findAll(Pageable pageable) {
+        Page<EventDTO> list = service.findAll(pageable);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @PostMapping
+    public ResponseEntity<EventDTO> insert(@RequestBody EventDTO dto) {
+        EventDTO eventDTO = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+                .buildAndExpand(eventDTO.getId()).toUri();
+        return ResponseEntity.created(uri).body(eventDTO);
+    }
+}
