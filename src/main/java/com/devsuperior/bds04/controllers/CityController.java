fba4919a084e08d5102865ac1736ed1db3e2149b
Breno <breno.joaosalvadorperez200@gmail.com>
2026-09-15 10:05:36 -0300

    feat: add CityController insert endpoint

---
+++ src/main/java/com/devsuperior/bds04/controllers/CityController.java
@@ -0,0 +1,30 @@
+package com.devsuperior.bds04.controllers;
+
+import com.devsuperior.bds04.dto.CityDTO;
+import com.devsuperior.bds04.services.CityService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(value = "/cities")
+public class CityController {
+
+    @Autowired
+    private CityService service;
+
+    @PostMapping
+    public ResponseEntity<CityDTO> insert(@Valid @RequestBody CityDTO dto) {
+        CityDTO cityDTO = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+                .buildAndExpand(cityDTO.getId()).toUri();
+        return ResponseEntity.created(uri).body(cityDTO);
+    }
+}
--- src/main/java/com/devsuperior/bds04/dto/CityDTO.java
+++ src/main/java/com/devsuperior/bds04/dto/CityDTO.java
@@ -10,7 +10,7 @@ public class CityDTO implements Serializable {
 	
 	private Long id;
 
-	@NotBlank(message = "Nome não pode ser vazio")
+	@NotBlank(message = "Campo requerido")
 	private String name;
 	
 	public CityDTO() {
