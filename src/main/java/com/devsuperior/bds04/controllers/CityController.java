848db2cb4658ced5cd2c086730df0a850d310c5e
Breno <breno.joaosalvadorperez200@gmail.com>
2026-09-15 11:09:47 -0300

    add OAuth2 authorization and resource server configuration

--- src/main/java/com/devsuperior/bds04/controllers/CityController.java
+++ src/main/java/com/devsuperior/bds04/controllers/CityController.java
@@ -5,6 +5,7 @@ import com.devsuperior.bds04.services.CityService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -18,6 +19,7 @@ public class CityController {
     @Autowired
     private CityService service;
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     @PostMapping
     public ResponseEntity<CityDTO> insert(@Valid @RequestBody CityDTO dto) {
         CityDTO cityDTO = service.insert(dto);
--- src/main/java/com/devsuperior/bds04/controllers/EventController.java
+++ src/main/java/com/devsuperior/bds04/controllers/EventController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -24,6 +25,7 @@ public class EventController {
         return ResponseEntity.ok().body(list);
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
     @PostMapping
     public ResponseEntity<EventDTO> insert(@RequestBody EventDTO dto) {
         EventDTO eventDTO = service.insert(dto);
